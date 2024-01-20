0ae2d07c43e4388ec7fbb2d122d0349c305ba557
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2024-01-20 13:41:35 -0500

    feat: next-auth-example login functionality

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -1,9 +1,49 @@
-"use client";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import { auth } from "@/auth"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { SignIn, SignOut } from "@/components/auth-components"
 
-export default function Login() {
+export default async function Login() {
+  const session = await auth()
+  if (!session?.user) return <SignIn />
   return (
-    <main>
-      <span>Login</span>
-    </main>
-  );
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative w-8 h-8 rounded-full">
+          <Avatar className="w-8 h-8">
+            {session.user.image && (
+              <AvatarImage
+                src={session.user.image}
+                alt={session.user.name ?? ""}
+              />
+            )}
+            <AvatarFallback>{session.user.email}</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">
+              {session.user.name}
+            </p>
+            <p className="text-xs leading-none text-muted-foreground">
+              {session.user.email}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuItem>
+          <SignOut />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
 }
+
