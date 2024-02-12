5677564591e3bbcdfbf8bd2b857c0dad4b6d53b0
MasonBoom <masonboom@gmail.com>
2024-02-11 20:25:49 -0500

    header + footer conditional rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,25 @@
 "use client"
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import React, { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [showHeaderFooter, setShowHeaderFooter] = useState(true);
+  const pathname = usePathname();
 
-const metadata: Metadata = {
-  title: "LingoListen AI - Language Learning Assistant",
-  description:
-    "A web app that allows learners to speak to a bot to learn how to hold conversations in foreign languages",
-};
+  useEffect(() => {
+    const noHeaderFooterPages = ["/", "/Login", "/SignUp", "/LanguageSelection"];
+    setShowHeaderFooter(!noHeaderFooterPages.includes(pathname));
+  }, [pathname]);
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  
   return (
     <html lang="en">
       <body>
+        {showHeaderFooter && <Header />}
         <main>{children}</main>
-        <Footer />
+        {showHeaderFooter && <Footer />}
       </body>
     </html>
   );
