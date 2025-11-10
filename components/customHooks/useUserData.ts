8a9a4e4a07a43d1fb0312653c7851d9af5f49201
MasonBoom <masonboom@gmail.com>
2025-11-10 17:15:32 -0500

    fixed redirect issues

--- components/customHooks/useUserData.ts
+++ components/customHooks/useUserData.ts
@@ -3,9 +3,9 @@ import { usePathname, useRouter } from "next/navigation";
 import axios from "axios";
 
 const useUserData = () => {
-  const [userData, setUserData] = useState<any>(null);
+  const [userData, setUserData] = useState({} as any);
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
+  const [error, setError] = useState("");
   const pathname = usePathname();
   const router = useRouter();
 
@@ -14,25 +14,31 @@ const useUserData = () => {
       try {
         const response = await axios.get("/api/getUserData");
         setUserData(response.data);
+        setIsLoading(false);
       } catch (error: any) {
         if (axios.isAxiosError(error) && error.response?.status === 401) {
-          const nonRedirectPaths = ["/", "/Login", "/SignUp", "/ForgotPassword"];
+          const nonRedirectPaths = [
+            "/",
+            "/Login",
+            "/SignUp",
+            "/ForgotPassword",
+          ];
 
           if (!nonRedirectPaths.includes(pathname)) {
+            setError("Unauthorized");
             router.push("/Login");
           } else {
-            setError("Unauthorized: please log in to access your account.");
+            setError("Unauthorized access to restricted page");
           }
         } else {
           setError(error.message || "An error occurred");
         }
-      } finally {
         setIsLoading(false);
       }
     };
 
     fetchData();
-  }, [pathname]);
+  }, [pathname]); 
 
   return { userData, isLoading, error };
 };
